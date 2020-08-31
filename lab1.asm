ac7f82a2ae23baeb9d5bce03b1e52c438aa43563
conan-exploits <yanhaywire@gmail.com>
2020-08-31 20:33:13 +0300

    i have initiliazed variables

---
+++ lab1.asm
@@ -0,0 +1,20 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+x dw ?
+y dw ?
+m dw 2
+c dw 5
+ 
+
+start:
+    printn 'Enter a value of x'
+   
+ret
+
+
+
+
