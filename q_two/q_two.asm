369d3910f962bed2bf53336d7472457052860b4c
Nkatha-Imanene <maureenimanene@gmail.com>
2020-09-11 17:38:12 -0700

    print y solution

--- q_two/q_two.asm
+++ q_two/q_two.asm
@@ -1,24 +1,3 @@
-org 100h
-
-x dw ?
-y dw ?
-m dw 2
-c dw 5
- 
-
-start:
-print 'Enter a value of x '
-   
-call scan_num            ; +++ scanned number stored in CX
-mov x, CX                
-putc 10
-putc 13
-
-mov AX, m                 ; 
-mul x 
-; ADD
-add AX,c
-
 print ", of y= "
 call print_num 
 
